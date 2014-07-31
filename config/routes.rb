7dc1c5b15b282c1aac9c314233e696e43119ff0b
Jack Hou <jack.hou@Live.com>
2014-07-30 21:21:30 -0700

    Add User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Fudge::Application.routes.draw do
 
+  get "users/new"
+
   root "static_pages#home"
 
   match '/about',    to: 'static_pages#about',    via: 'get'
+  match '/signup',   to: 'users#new', via: 'get'
   match '/faq',    to: 'static_pages#faq',    via: 'get'
   match '/contact',    to: 'static_pages#contact',    via: 'get'
 
